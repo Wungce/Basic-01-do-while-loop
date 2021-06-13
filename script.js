91894ded1d841fa6ea33386232692e2e8bc1bcee
wangsh <wugcelama19@gmail.com>
2021-06-13 14:33:41 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//function create.
+function itsMe(number){
+
+    let i =1;
+     
+    //while loop using here.
+    do{
+
+        console.log(i);
+
+
+        i++
+    }
+    while(i <= number)
+
+
+
+
+}
+
+//fucntio call .
+itsMe(12);
